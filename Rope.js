a6d59f2ccf4baed6fdf4b0b633b10bc9dba1ff1a
milonee608 <70271042+milonee608@users.noreply.github.com>
2020-10-09 00:39:44 +0530

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,22 @@
+class Rope{
+    constructor(bodyA,bodyB,offsetX,offsetY){
+        this.offsetX=offsetX;
+        this.offsetY=offsetY; 
+       var options={
+           bodyA: bodyA,
+           bodyB: bodyB,
+           pointB:{x:this.offsetX,y:this.offsetY}
+        
+       }
+       this.rope=Constraint.create(options);
+       World.add(world,this.rope);
+    }
+    display(){
+        var posA=this.rope.bodyA.position;
+        var posB=this.rope.bodyB.position;
+
+        stokeWeight(2);
+
+        line(posA.x, posA.y ,posB.x+this.offsetX, posB.y+this.offsetY);
+    }
+}
